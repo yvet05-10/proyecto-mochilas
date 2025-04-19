3a99ea97dcdd624e1c044f303dfe1ca26e660407
christopher leyva <cris_lg_al@hotmail.com>
2025-04-18 23:34:34 -0500

    cargando proyecto

---
+++ ProyectoMochilas/src/gui/MenuPrincipal.java
@@ -0,0 +1,48 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+
+public class MenuPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuPrincipal frame = new MenuPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox cbopaises = new JComboBox();
+		cbopaises.setBounds(79, 10, 153, 21);
+		contentPane.add(cbopaises);
+		
+	}
+
+}
